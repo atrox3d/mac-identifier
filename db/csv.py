714532b3711501cff7b5b522712da4534cba1b07
atrox3d <atrox3d@gmail.com>
2025-12-29 17:14:04 +0100

    fix all tests

--- db/csv.py
+++ db/csv.py
@@ -27,12 +27,13 @@ def load_txt_csv(
     return df
 
 
-def get_dhcp_table(
+def get_mac_ref_table(
     df:pd.DataFrame,
     # ip_column:int|str,
-    mac_column:int|str,
-    mac_sep:str,
-    mac_replace_sep:str=':'
+    descr_col:int|str,
+    mac_col:int|str,
+    sep:str,
+    replace:str=':'
 ) -> pd.DataFrame:
     """
     Extracts and cleans IP and MAC columns from a DataFrame.
@@ -41,38 +42,17 @@ def get_dhcp_table(
         ip_column: Name (str) or index (int) of the IP column.
         mac_column: Name (str) or index (int) of the MAC column.
     """
-    # --- Robust Column Selection ---
-    # Validate and resolve column identifiers to actual names, providing clear errors.
-    # column_names = []
-    # for i, col_ref in enumerate([ip_column, mac_column]):
-    #     col_type_str = "IP" if i == 0 else "MAC"
-    #     try:
-    #         if isinstance(col_ref, int):
-    #             column_name = df.columns[col_ref]
-    #         elif isinstance(col_ref, str):
-    #             if col_ref not in df.columns:
-    #                 raise KeyError(f"Column name '{col_ref}' not found in {list(df.columns)}")
-    #             column_name = col_ref
-    #         else:
-    #             raise TypeError(f"Identifier must be an int or str, not {type(col_ref).__name__}.")
-    #         column_names.append(column_name)
-    #     except (IndexError, KeyError, TypeError) as e:
-    #         raise ValueError(f"Invalid {col_type_str} column identifier provided.") from e
-
-    # if column_names[0] == column_names[1]:
-    #     raise ValueError(f"IP and MAC columns cannot be the same: '{column_names[0]}'")
-    
-    # Select the desired columns and explicitly create a copy.
-    # This prevents the pandas SettingWithCopyWarning, which occurs when
-    # modifying a slice of a DataFrame whose state (view vs. copy) is ambiguous.
-    # By using .copy(), we guarantee we are working with a new, independent DataFrame.
-    # df = df.copy()
-    
-    # ip_col, mac_col = column_names
-    mac_col = df.columns[mac_column] if isinstance(mac_column, int) else mac_column
-    df[mac_col] = df[mac_col].str.replace(
-        mac_sep,
-        mac_replace_sep,
+    # mac_col = df.columns[mac_column] if isinstance(mac_column, int) else mac_column
+    cols = [
+        df.columns[col] if isinstance(col, int) else col
+        for col in [descr_col, mac_col]
+    ]
+    descr, mac = cols
+    df[mac] = df[mac].str.replace(
+        sep,
+        replace,
         regex=False
     )
+    df = df[[mac, descr]]
+    df.columns = ['mac', 'name']
     return df
--- tests/db/test_csv.py
+++ tests/db/test_csv.py
@@ -20,67 +20,79 @@ def secret_dir() -> Path:
 
 
 @pytest.fixture
-def router_txt_path(secret_dir) -> Path:
+def router_txt_path(secret_dir: Path) -> Path:
     path = secret_dir / ROUTER_TXT
     # print(path)
     return path
 
 
 @pytest.fixture
-def mymacs_txt_path(secret_dir) -> Path:
+def mymacs_txt_path(secret_dir: Path) -> Path:
     path = secret_dir / MY_MACS
     # print(path)
     return path
 
 
 @pytest.fixture
-def hismacs_txt_path(secret_dir) -> Path:
+def hismacs_txt_path(secret_dir: Path) -> Path:
     path = secret_dir / HIS_MACS
     # print(path)
     return path
 
 
-def test_load_txt_csv(router_txt_path):
+def test_load_txt_csv(router_txt_path: Path):
     df = csv.load_txt_csv(router_txt_path)
     print(df.columns)
     print(df)
-    print(df.count())
+    print(df.shape)
+    assert df.shape == (11, 3)
+    assert list(df.columns) == ['IPv4', 'MAC', 'Dispositivo']
 
 
-def test_get_dhcp_table(router_txt_path):
+def test_get_dhcp_table(router_txt_path: Path):
     df = csv.load_txt_csv(router_txt_path)
-    df = csv.get_dhcp_table(df, 1, ':', '-')
+    df = csv.get_mac_ref_table(df, descr_col=1, mac_col=0, sep=':', replace='-')
     print(df.columns)
     print(df)
     # Explicitly filter for True values and then count them
-    matches = df.iloc[:, 1].str.contains(':')
+    matches = df.iloc[:, 0].str.contains(':')
     print(matches[matches == True].count())
     assert matches[matches == True].count() == 0
 
 
-
-
-def test_load_mymacs(mymacs_txt_path):
+def test_load_mymacs(mymacs_txt_path: Path):
     df = csv.load_txt_csv(mymacs_txt_path, separator=';')
     print(df.columns)
     print(df)
+    print(df.shape)
+    assert df.shape == (14, 2)
+    assert list(df.columns) == ['mac', 'description']
 
 
-def test_get_mymacs_table(mymacs_txt_path):
+def test_get_mymacs_table(mymacs_txt_path: Path):
     df = csv.load_txt_csv(mymacs_txt_path, separator=';')
-    df = csv.get_dhcp_table(df, 0, '-', ':')
+    df = csv.get_mac_ref_table(df, descr_col=1, mac_col=0, sep='-', replace=':')
     print(df.columns)
     print(df)
+    matches = df.iloc[:, 0].str.contains('-')
+    print(matches[matches == True].count())
+    assert matches[matches == True].count() == 0
 
 
-def test_load_hismacs(hismacs_txt_path):
+def test_load_hismacs(hismacs_txt_path: Path):
     df = csv.load_txt_csv(hismacs_txt_path, separator=';')
     print(df.columns)
     print(df)
+    print(df.shape)
+    assert df.shape == (11, 2)
+    assert list(df.columns) == ['mac', 'description']
 
 
-def test_get_hismacs_table(hismacs_txt_path):
+def test_get_hismacs_table(hismacs_txt_path: Path):
     df = csv.load_txt_csv(hismacs_txt_path, separator=';')
-    df = csv.get_dhcp_table(df, 0, '-', ':')
+    df = csv.get_mac_ref_table(df, descr_col=1, mac_col=0, sep='-', replace=':')
     print(df.columns)
     print(df)
+    matches = df.iloc[:, 0].str.contains('-')
+    print(matches[matches == True].count())
+    assert matches[matches == True].count() == 0
