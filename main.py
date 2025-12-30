3ea16650e44d1551ba98ac0fcbeea0140ea8c8ac
atrox3d <atrox3d@gmail.com>
2025-12-30 14:52:55 +0100

    comment main

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ app = Typer()
 
 @app.command()
 def main(
-    secret_dir:str=None,
-    router_filename:str=None,
-    known_macs_filename:str=None,
-    unknown_macs_filename:str=None
+    secret_dir:str=None,            # overrides SECRET_DIR
+    router_filename:str=None,       # overrides ROUTER_DHCP_FILENAME
+    known_macs_filename:str=None,   # overrides KNOWN_MACS_FILENAME
+    unknown_macs_filename:str=None  # overrides UNKNOWN_MACS_FILENAME
 ):
+    # try to load .env
     if Path(".env").exists():
         CONFIG = dotenv_values(".env")
     else:
         raise FileNotFoundError(".CONFIG file not found")
 
+    # try to evaluate config
     try:
         SECRET_DIR            = secret_dir or CONFIG['SECRET_DIR']
         SECRET_PATH           = Path(SECRET_DIR).resolve()
@@ -33,29 +35,31 @@ def main(
         UNKNOWN_MACS_FILENAME = unknown_macs_filename or  CONFIG['UNKNOWN_MACS_FILENAME']
         UNKNOWN_MACS_PATH     = SECRET_PATH / UNKNOWN_MACS_FILENAME
     except KeyError as ke:
+        # could not resolve a particular config value
         print(f'cannot resolve value for config key {ke}')
         exit(1)
     
+    # build router dhcp table
     router_dhcp_df = csv.load_csv(ROUTER_DHCP_PATH)
     router_dhcp_df = csv.build_mac_ref_table(router_dhcp_df, descr_col=0, mac_col=1, sep=':', replace=':', name_col_name='ip')
-    # print(router_dhcp_df)
 
+    # build known macs table
     known_macs_df = csv.load_csv(KNOWN_MACS_PATH, separator=';')
     known_macs_df = csv.build_mac_ref_table(known_macs_df, descr_col=1, mac_col=0, sep='-', replace=':')
-    # print(known_macs_df)
 
+    # build unknown macs table
     unknown_macs_df = csv.load_csv(UNKNOWN_MACS_PATH, separator=';')
     unknown_macs_df = csv.build_mac_ref_table(unknown_macs_df, descr_col=1, mac_col=0, sep='-', replace=':')
-    # print(unknown_macs_df)
     
+    # build stacked macs table
     stacked = csv.stack_mac_ref_tables(known_macs_df, unknown_macs_df)
-    # print(stacked)
 
+    # resolve macs
     merged = csv.merge_and_fill(
         router_dhcp_df, 
         stacked, 
-        on='mac', fill_col='name')
-    # print(merged.columns)
+        on='mac', fill_col='name'
+    )
     print(merged)
 
 
