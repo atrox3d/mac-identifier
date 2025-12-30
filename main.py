69bcdd1d773d555eae9cad2e9a8457dadec08cd2
atrox3d <atrox3d@gmail.com>
2025-12-30 14:43:46 +0100

    add typer

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 from pathlib import Path
+
 from dotenv import dotenv_values
+from typer import Typer
+
 from db import csv
 
 
-def main():
+app = Typer()
+
+@app.command()
+def main(
+    secret_dir:str=None,
+    router_filename:str=None,
+    known_macs_filename:str=None,
+    unknown_macs_filename:str=None
+):
     if Path(".env").exists():
         CONFIG = dotenv_values(".env")
     else:
         raise FileNotFoundError(".CONFIG file not found")
 
-    SECRET_DIR            = CONFIG.get('SECRET_DIR')
-    SECRET_PATH           = Path(SECRET_DIR).resolve()
+    try:
+        SECRET_DIR            = secret_dir or CONFIG['SECRET_DIR']
+        SECRET_PATH           = Path(SECRET_DIR).resolve()
 
-    ROUTER_DHCP_FILENAME  = CONFIG.get('ROUTER_DHCP_FILENAME')
-    ROUTER_DHCP_PATH      = SECRET_PATH / ROUTER_DHCP_FILENAME
+        ROUTER_DHCP_FILENAME  = router_filename or CONFIG['ROUTER_DHCP_FILENAME']
+        ROUTER_DHCP_PATH      = SECRET_PATH / ROUTER_DHCP_FILENAME
 
-    KNOWN_MACS_FILENAME   = CONFIG.get('KNOWN_MACS_FILENAME')
-    KNOWN_MACS_PATH       = SECRET_PATH / KNOWN_MACS_FILENAME
+        KNOWN_MACS_FILENAME   = known_macs_filename or CONFIG['KNOWN_MACS_FILENAME']
+        KNOWN_MACS_PATH       = SECRET_PATH / KNOWN_MACS_FILENAME
 
-    UNKNOWN_MACS_FILENAME = CONFIG.get('UNKNOWN_MACS_FILENAME')
-    UNKNOWN_MACS_PATH     = SECRET_PATH / UNKNOWN_MACS_FILENAME
-
+        UNKNOWN_MACS_FILENAME = unknown_macs_filename or  CONFIG['UNKNOWN_MACS_FILENAME']
+        UNKNOWN_MACS_PATH     = SECRET_PATH / UNKNOWN_MACS_FILENAME
+    except KeyError as ke:
+        print(f'cannot resolve value for config key {ke}')
+        exit(1)
+    
     router_dhcp_df = csv.load_csv(ROUTER_DHCP_PATH)
     router_dhcp_df = csv.build_mac_ref_table(router_dhcp_df, descr_col=0, mac_col=1, sep=':', replace=':', name_col_name='ip')
     # print(router_dhcp_df)
@@ -46,4 +61,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    #
+    app()
